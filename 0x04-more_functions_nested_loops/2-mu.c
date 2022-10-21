2e3b374872f544c49147fd383a310ae0ad904642
Emmanuel Alochukwu Ucha <emmanuelrichards600@gmail.com>
2022-10-20 17:46:57 -0700

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mu.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - function that mutiplies two integer
+ * @a: first integer
+ * @b: second integer
+ * Return: always 0 success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
