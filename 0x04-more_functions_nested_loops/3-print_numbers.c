406f6ff631b5ff93cfa2599654ee2fa76862472d
Emmanuel Alochukwu Ucha <emmanuelrichards600@gmail.com>
2022-10-20 17:50:22 -0700

    Commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - function that print numbers from 0 - 9
+* Return: void
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
