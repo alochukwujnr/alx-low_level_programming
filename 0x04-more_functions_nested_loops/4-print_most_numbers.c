6302f589224bbb0725b129a564110faab171b3bc
Emmanuel Alochukwu Ucha <emmanuelrichards600@gmail.com>
2022-10-20 18:00:09 -0700

    Commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers
- * Return:0
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4, followed by a new line
  */
-
 void print_most_numbers(void)
 {
-	char n = 0;
+	int i;
 
-	while (n <= 9)
+	for (i = 0; i < 10; i++)
 	{
-		if (n != 2 && n != 4)
+		if (i != 2 && i != 4)
 		{
-			_putchar('0' + n);
+			_putchar(i + '0');
 		}
-
-		n++;
 	}
-        {
+
 	_putchar('\n');
-}
 }
